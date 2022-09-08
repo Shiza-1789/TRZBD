34c13506c9d6f6bc2c5f05c1856e061736571b60
Nika <Veronikaav12@yandex.ru>
2022-09-08 14:39:58 +0300

    lab1

---
+++ WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.byFileButton = new System.Windows.Forms.Button();
+            this.gridOfNumbers = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // byFileButton
+            // 
+            this.byFileButton.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.byFileButton.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.byFileButton.Location = new System.Drawing.Point(487, 28);
+            this.byFileButton.Name = "byFileButton";
+            this.byFileButton.Size = new System.Drawing.Size(146, 58);
+            this.byFileButton.TabIndex = 7;
+            this.byFileButton.Text = "Добавление из файла";
+            this.byFileButton.UseVisualStyleBackColor = false;
+            this.byFileButton.Click += new System.EventHandler(this.byFileButton_Click);
+            // 
+            // gridOfNumbers
+            // 
+            this.gridOfNumbers.Location = new System.Drawing.Point(12, 28);
+            this.gridOfNumbers.Name = "gridOfNumbers";
+            this.gridOfNumbers.Size = new System.Drawing.Size(403, 166);
+            this.gridOfNumbers.TabIndex = 8;
+            this.gridOfNumbers.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 297);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(269, 80);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(621, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 58);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Поиск";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(372, 340);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(188, 37);
+            this.richTextBox2.TabIndex = 11;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(487, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 58);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Палиндромы";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 250);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 23);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Вывод результата:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(369, 295);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 32);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Введите слово для поиска:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.gridOfNumbers);
+            this.Controls.Add(this.byFileButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button byFileButton;
+        private System.Windows.Forms.RichTextBox gridOfNumbers;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1() //форма создается
+        {
+            InitializeComponent();
+
+            FindPolly("adada");
+        }
+
+        public string FindMyText(string text)
+        {            
+            string returnValue = (" ");
+      
+            if (text.Length > 0)
+            {
+                // Получить расположение строки поиска в richTextBox1.
+                int indexToText = gridOfNumbers.Find(text);
+                // Определяем, был ли текст найден в richTextBox1.
+                if (indexToText >= 0)
+                {
+                    returnValue = richTextBox1.Text;
+                }
+            }
+
+            return returnValue;
+        }
+
+
+
+        private void byFileButton_Click(object sender, EventArgs e)// взятие массива из файла
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            dialog.InitialDirectory = "c:\\";
+            dialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            dialog.FilterIndex = 2;
+            dialog.RestoreDirectory = true;
+
+            var fileContent = string.Empty;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var fileStream = dialog.OpenFile();
+
+                using (StreamReader reader = new StreamReader(fileStream)) //считывание из файла 
+                {
+                    fileContent = reader.ReadToEnd(); //записываем в fileContent
+                }
+
+                gridOfNumbers.Text = fileContent;
+            }
+        }
+
+
+
+
+        private string FindPolly(string word)
+        {
+            string firstHalf = "";
+            string secondHalf = "";
+
+            if (word.Length % 2 == 0)
+            {
+                for (int i = 0; i < word.Length / 2; i++)
+                {
+                    firstHalf += word[i];
+                }
+                for (int i = word.Length - 1; i >= word.Length / 2; i--)
+                {
+                    secondHalf += word[i];
+                }
+
+                if (firstHalf == secondHalf)
+                {
+                    return word;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < word.Length / 2; i++)
+                {
+                    firstHalf += word[i];
+                }
+                for (int i = word.Length - 1; i > word.Length / 2; i--)
+                {
+                    secondHalf += word[i];
+                }
+
+                if (firstHalf == secondHalf)
+                {
+                    return word;
+                }
+            }
+
+            return "";
+        }
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string word = richTextBox2.Text;
+
+            string row = gridOfNumbers.Text;
+
+            string[] lol = gridOfNumbers.Text.Split();
+
+            int k = 0;
+
+            for (int i = 0; i < lol.Length; i++)
+            {
+               
+                if (lol[i] == word)
+                {
+                    k++;
+                }
+
+            }
+
+            richTextBox1.Text = k.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string result = "";
+
+            string[] lol = gridOfNumbers.Text.Split();
+
+            for (int i = 0; i < lol.Length; i++)
+            {
+                result += FindPolly(lol[i]) + " ";
+            }
+
+            richTextBox1.Text = result;
+        }
+    }
+}
